b9dd59a4bfd891302c750422e9fd5a34f864ff8d
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2023-07-07 14:43:43 +0300

    feat: add generate_array test
    sorting_algorithms

--- sorting_algorithms/src/generate_array/generate_array.rs
+++ sorting_algorithms/src/generate_array/generate_array.rs
@@ -1,9 +1,17 @@
-use rand::prelude::*;
+use rand::{prelude::SliceRandom, thread_rng};
 
 pub fn generate_array(n: i32) -> Vec<i32> {
-	let mut rng = rand::thread_rng();
-
 	let mut nums: Vec<i32> = (1..n).collect();
-	nums.shuffle(&mut rng);
+	nums.shuffle(&mut thread_rng());
 	nums
+}
+
+#[cfg(test)]
+mod tests {
+	use super::generate_array;
+	#[test]
+	fn test_generate_array() {
+		let array = generate_array(5);
+		assert_eq!(array.len(), vec![1, 2, 3, 4].len());
+	}
 }
